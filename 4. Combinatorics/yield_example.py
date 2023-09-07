1d2e9bcc7963c58b0117e11271f7b7a777a5acea
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-09-07 14:53:08 -0600

    Add code for yield demo

---
+++ 4. Combinatorics/yield_example.py
@@ -0,0 +1,36 @@
+from collections.abc import Iterator
+
+def numbers() -> Iterator[int]:
+    x = 1
+    yield x
+    x *= 3
+    yield x
+    x += 2
+    yield x
+
+def pow2(n: int) -> Iterator[int]:
+    i = 0
+    while True:
+        yield 2 ** i
+        i += 1
+
+if __name__ == '__main__':
+    g1 = numbers()
+    print(f'{next(g1) = }')
+    print(f'{next(g1) = }')
+    print(f'{next(g1) = }')
+    # print(f'{next(g1) = }')
+    for w in numbers():
+        print(f'{w = }')
+    g2 = pow2(5)
+    print(f'{next(g2) = }')
+    print(f'{next(g2) = }')
+    print(f'{next(g2) = }')
+    print(f'{next(g2) = }')
+    print(f'{next(g2) = }')
+    print(f'{next(g2) = }')
+
+    for u in pow2(0):
+        print(f'{u = }')
+        if u > 1_000_000:
+            break
