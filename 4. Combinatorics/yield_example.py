5b610e4d8adc406372324a40158e7c2d9deb9bd9
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-09-07 14:53:35 -0600

    Apply code format

--- 4. Combinatorics/yield_example.py
+++ 4. Combinatorics/yield_example.py
@@ -1,5 +1,6 @@
 from collections.abc import Iterator
 
+
 def numbers() -> Iterator[int]:
     x = 1
     yield x
@@ -8,11 +9,13 @@ def numbers() -> Iterator[int]:
     x += 2
     yield x
 
+
 def pow2(n: int) -> Iterator[int]:
     i = 0
     while True:
         yield 2 ** i
         i += 1
+
 
 if __name__ == '__main__':
     g1 = numbers()
