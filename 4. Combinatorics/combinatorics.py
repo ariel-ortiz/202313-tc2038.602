1cc24add38b804ed39cfac57206269eae2b19f0e
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-09-21 14:32:16 -0600

    Add code to implement permutations

--- 4. Combinatorics/combinatorics.py
+++ 4. Combinatorics/combinatorics.py
@@ -21,9 +21,34 @@ def combinations(s: list[C], k: int) -> list[list[C]]:
     return [t for t in power_set(s) if len(t) == k]
 
 
+def insert(x: C, s: list[C], i: int) -> list[C]:
+    return s[:i] + [x] + s[i:]
+
+
+def insert_everywhere(x: C, s: list[C]) -> list[list[C]]:
+    return [insert(x, s, i) for i in range(len(s) + 1)]
+
+
+def permute(s: list[C]) -> list[list[C]]:
+    if s:
+        return sum([insert_everywhere(s[0], t)
+                    for t in permute(s[1:])],
+                   [])
+    else:
+        return [[]]
+
+
+def permutations(s: list[C], k: int) -> list[list[C]]:
+    return sum([permute(t)
+                for t in combinations(s, k)],
+               [])
+
+
 if __name__ == '__main__':
     from pprint import pprint
     # pprint(sorted_nicely(power_set([1, 2, 3, 4])))
     # print()
     # pprint(sorted_nicely(power_set(['a', 'b', 'c'])))
-    pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 3)))
+    # pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 3)))
+    # pprint(insert_everywhere('a', ['b', 'c', 'd', 'e']))
+    pprint(sorted_nicely(permutations(['a', 'b', 'c', 'd'], 3)))
