8dab93e73d06a1b53012ddd58eeb5e6a8dce552c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-10-05 14:36:24 -0600

    Add files for the metrorrey graph problem

---
+++ 7. Graphs/metrorrey/metrorrey.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from graph import Graph
+from generic_search import bfs, Node, node_to_path
+
+if __name__ == '__main__':
+    metrorrey: Graph[str] = Graph(
+                                ["Talleres",
+                                 "San Bernabé",
+                                 "Unidad Modelo"])
+    metrorrey.add_edge_by_vertices("Talleres",
+                                   "San Bernabé")
+    metrorrey.add_edge_by_vertices("San Bernabé",
+                                   "Unidad Modelo")
+    result: Optional[Node[str]] = bfs(
+        "Talleres",
+        lambda x: x == "Unidad Modelo",
+        metrorrey.neighbors_for_vertex
+    )
+    if result:
+        path: list[str] = node_to_path(result)
+        print(path)
+    else:
+        print("No solution found")
